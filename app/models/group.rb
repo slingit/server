6851b8b57a5a07fe9baadc0ae793b4f6618b3784
Ross Penman <ross@pen.mn>
2015-01-02 19:55:19 +0000

    Group has many devices

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,9 @@
 class Group
+  include ActiveModel::Model
+
+  attr_accessor :id
+
+  def devices
+    Device.where(group_id: id)
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Group, :type => :model do
+  it { is_expected.not_to respond_to :save }
+
+  it "has an id" do
+    id = SecureRandom.uuid
+    group = Group.new(id: id)
+    expect(group.id).to eq id
+  end
+
+  it "finds devices" do
+    device = FactoryGirl.create(:device)
+    group = Group.new(id: device.group_id)
+    expect(group.devices).to include device
+    Device.where(group_id: group.id).destroy_all
+    expect(group.devices.reload).to be_empty
+  end
 end
